eccb2774bf272eec1d728ed3a860bc93b7444660
Dustin Murdock <dmurdock2@gmail.com>
2020-05-26 00:24:44 -0500

    finished Login and Register components

--- ecommerceapi/models/customer.py
+++ ecommerceapi/models/customer.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.urls import reverse
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 class Customer(models.Model):
@@ -12,16 +9,3 @@ class Customer(models.Model):
 
     
 
-# Every time a `User` is created, a matching `Customer`
-# object will be created and attached as a one-to-one
-# property
-@receiver(post_save, sender=User)
-def create_Customer(sender, instance, created, **kwargs):
-    if created:
-        Customer.objects.create(user=instance)
-
-# Every time a `User` is saved, its matching `Customer`
-# object will be saved.
-@receiver(post_save, sender=User)
-def save_Customer(sender, instance, **kwargs):
-    instance.customer.save()
