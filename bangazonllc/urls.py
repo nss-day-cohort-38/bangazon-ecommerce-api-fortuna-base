9a7745432379e9b2bb83148fbda9596f774a545c
Cody Murdock <warhammer4563@gmail.com>
2020-05-27 13:57:30 -0500

    Payment information listed

--- bangazonllc/urls.py
+++ bangazonllc/urls.py
@@ -18,8 +18,12 @@ from django.urls import path, include
 from rest_framework import routers
 from ecommerceapi.views import register_user, login_user
 from rest_framework.authtoken.views import obtain_auth_token
+from ecommerceapi.views import *
+
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'payment_types', PaymentTypes, 'payment_types')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- ecommerceapi/views/__init__.py
+++ ecommerceapi/views/__init__.py
@@ -1,1 +1,2 @@
 from .register import register_user, login_user
+from .payment_type import PaymentTypes
