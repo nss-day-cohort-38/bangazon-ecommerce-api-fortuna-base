2b7e0ae74b01ce94932ad857fb74366727eb7043
Dustin Murdock <dmurdock2@gmail.com>
2020-06-02 14:24:01 -0500

    register product_types

--- ecommerceapi/admin.py
+++ ecommerceapi/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer,Product_Type
 
 # Register your models here.
 admin.site.register(Customer)
+admin.site.register(Product_Type)
