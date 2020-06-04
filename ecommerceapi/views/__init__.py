7736263fd291a807766887dd55075493de975796
Dustin Murdock <dmurdock2@gmail.com>
2020-06-04 14:33:48 -0500

    fix merge conflict

--- ecommerceapi/views/__init__.py
+++ ecommerceapi/views/__init__.py
@@ -2,8 +2,5 @@ from .register import register_user, login_user
 from .product_type import Product_Types
 from .payment_type import PaymentTypes
 from .product import Products
-<<<<<<< HEAD
 from .order import Orders
-=======
 from .customer import Customers
->>>>>>> d7f4289f25adc18a2c7a4156b8c2d0e7f305c16b
