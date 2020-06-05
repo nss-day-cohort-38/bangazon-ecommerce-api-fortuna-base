c8ac61398c6b3b9952d990d70dc6035a44fa2524
ChaseSully222 <Jcsullins222@yahoo.com>
2020-06-05 13:18:21 -0500

    updated the order product model

--- ecommerceapi/models/order.py
+++ ecommerceapi/models/order.py
@@ -8,7 +8,7 @@ class Order(models.Model):
 
     customer= models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
     payment_type = models.ForeignKey(Payment_Type, blank=True, null=True, on_delete=models.DO_NOTHING)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
     products = models.ManyToManyField("Product", through='Order_Product',)
 
     class Meta:
